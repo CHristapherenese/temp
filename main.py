f949e36e916c0fdf34f0322a08455c1fe168a3aa
CHristapherenese <64864560+CHristapherenese@users.noreply.github.com>
2020-05-11 13:40:31 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,48 +1,45 @@
-Temp = 0
-degrees = 0
-Temp3 = 0
-pins.LED.digital_write(False)
-ht16k33.set_address(HT16K33_I2C_ADDRESSES.ADD_0X70)
-ht16k33.set_blink_rate(5)
-ht16k33.set_brightness(100)
-_7seg = 0
-def on_forever():
-    global Temp3
-    if True:
-        for index in range(3):
-            Temp3 = 1
-            pause(1000)
-            Temp3 = 0
-            pause(1000)
-    pause(5000)
-forever(on_forever)
-def on_forever2():
-    global degrees, Temp, _7seg
-    degrees = pins.i2c_read_number(24, NumberFormat.INT8_LE, True)
-    console.log_value("deg", degrees)
+def temp_up():
+    global Temp
+    Temp = 0
+    console.log_value("Temp", Temp)
     pause(1000)
-    if degrees < -50:
-        Temp = 0
-        console.log_value("Temp", Temp)
+    Temp = 1
+    console.log_value("Temp", Temp)
+    pause(1000)
+    Temp = 2
+    console.log_value("Temp", Temp)
+    pause(1000)
+    Temp = 3
+    console.log_value("Temp", Temp)
+def CodeTemp3():
+    global Temp3
+    for index in range(3):
+        Temp3 = 1
         pause(1000)
-        Temp = 1
-        console.log_value("Temp", Temp)
+        Temp3 = 0
         pause(1000)
-        Temp = 2
-        console.log_value("Temp", Temp)
-        pause(1000)
-        Temp = 3
-        console.log_value("Temp", Temp)
-        _7seg = Temp
-        pins.i2c_write_number(113, _7seg, NumberFormat.UINT32_LE, True)
-        console.log_value("7Seg", _7seg)
-        pause(1000)
-forever(on_forever2)
-def on_forever3():
+    pause(5000)
     if Temp3:
         pins.LED.digital_write(True)
         pixel.set_color(0xff0000)
     else:
         pins.LED.digital_write(False)
         pixel.set_color(0x000000)
-forever(on_forever3)
+Temp = 0
+Temp3 = 0
+pins.LED.digital_write(False)
+ht16k33.set_address(HT16K33_I2C_ADDRESSES.ADD_0X70)
+ht16k33.set_blink_rate(5)
+ht16k33.set_brightness(100)
+CodeTemp3()
+def on_forever():
+    pass
+forever(on_forever)
+def on_forever2():
+    degrees = 0
+    if degrees == 0:
+        _7seg = 0
+        pins.i2c_write_number(113, _7seg, NumberFormat.UINT32_LE, True)
+        console.log_value("7Seg", _7seg)
+        pause(1000)
+forever(on_forever2)
